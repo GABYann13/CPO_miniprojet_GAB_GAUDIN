d15b6a8ff974de2790359d1a20ff92b3f70daa4b
kango <kango@10.3.62.115>
2024-11-27 15:31:06 +0100

    Mini projet Master Mind : création classe Combinaison

---
+++ PremierprojetTest/src/premierprojettest/Combinaison.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package premierprojettest;
+
+/**
+ *
+ * @author kango
+ */
+public class Combinaison {
+    Pion[] element;
+    int taille;
+    Pion [] combinaison = new Pion [4];
+            
+    public Combinaison(Pion[] elements){
+        
+    }
+    public genererAleatoire(int taille, ArrayList<Character> couleursDisponibles) {
+        
+    }
+    public comparer(Combinaison autre){
+        
+    }
+    toString(){
+        
+    }
+}
