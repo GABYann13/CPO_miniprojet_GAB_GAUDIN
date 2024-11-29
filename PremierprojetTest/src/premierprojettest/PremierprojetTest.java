086ed6b5747f4df460ffcbd492f2bb49ede1e9e5
yann <yann@10.3.62.35>
2024-11-29 12:33:27 +0100

    Amélioration de la classe Pion

--- PremierprojetTest/src/premierprojettest/PremierprojetTest.java
+++ PremierprojetTest/src/premierprojettest/PremierprojetTest.java
@@ -14,8 +14,18 @@ public class PremierprojetTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int a;
-        int b;
+       Pion pionRouge = new Pion("Rouge");
+        Pion pionBleu = new Pion("Bleu");
+        Pion pionViolet = new Pion("Violet");
+        Pion pionJaune = new Pion("Jaune");
+
+        
+        System.out.println("Pion Rouge : " + pionRouge);  
+        System.out.println("Pion Bleu : " + pionBleu);    
+        System.out.println("Pion Violet : " + pionViolet);
+        System.out.println("Pion Jaune : " + pionJaune);
+        
+            
     }
     
 }
