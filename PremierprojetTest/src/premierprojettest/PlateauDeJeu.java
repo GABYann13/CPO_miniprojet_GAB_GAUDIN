8991a4b0a1ea5bf381be8447f9d0cd592ee36787
kango <kango@10.3.62.115>
2024-12-08 00:59:19 +0100

    Mastermind : classe : plateau de jeu, tentative de resolution de la méthode proposerCombinaison

--- PremierprojetTest/src/premierprojettest/PlateauDeJeu.java
+++ PremierprojetTest/src/premierprojettest/PlateauDeJeu.java
@@ -5,6 +5,7 @@
 package premierprojettest;
 
 import java.util.ArrayList;
+import Combinaison.java;
 
 /**
  *
@@ -12,9 +13,25 @@ import java.util.ArrayList;
  */
 public class PlateauDeJeu {
     Combinaison CombinaisonDuMaitre;
-    ArrayList <Combinaison> tentatives;
+    ArrayList <Combinaison> Tentatives; // Garde en memoir les differentes combinaisons du joueuer
+    ArrayList <Character> UneCombinaison; // Creation d'une liste pour devenir la tentative du joueur 
      ArrayList <String> reponses;
      int nbToursMax;
+     int nbTentatives=0;
+     
+     int taille2 = combinaison.size(); // import de la classe Combinaison puis creation de la taille d'une combinaison
+     
+     //Creation d'une liste qui prends 4 couleures pour creer une tentative du joueur
+     
+     for (int i=0; i<taille2 ; i++ ){
+        
+        
+        
+    
+}
+     UneCombinaison.add();
+     
+     Tentatives.add(UneCombinaison);
 
     public PlateauDeJeu(Combinaison CombinaisonDuMaitre, int nbToursMax) {
         this.CombinaisonDuMaitre = CombinaisonDuMaitre;
@@ -23,7 +40,8 @@ public class PlateauDeJeu {
     
     public proposerCombinaison(Combinaison autre){
         if ("bouton validé apuuyé"){
-        nbToursMax = nbToursMax-1;
+        nbTentatives = nbTentatives+1;
+        
         
     }
     }
@@ -33,11 +51,14 @@ public class PlateauDeJeu {
     }
     
     public Victoire(){
-        
+       if (CombinaisonDuMaitre==Tentatives){
+           System.out.println("Congratulation ! You win !");
+       }
+ 
     }
      
     public Game_Over(){
-        if (nbToursMax==0){
+        if (nbTentatives==nbToursMax){
             System.out.println("Game Over");
         }
         
