455f0e406788a918e80a23502692a3f19984f8b2
yann <yann@10.3.62.35>
2024-12-11 11:27:50 +0100

    Changement dans pion

--- PremierprojetTest/src/premierprojettest/Pion.java
+++ PremierprojetTest/src/premierprojettest/Pion.java
@@ -1,5 +1,10 @@
 package premierprojettest;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JButton;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -32,6 +37,8 @@ public class Pion {
         }
         return ""; 
     }
+
+    
     
     
 }
