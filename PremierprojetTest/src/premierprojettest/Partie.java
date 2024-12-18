478e3117cbd09e42ebdbcf903c9fc18e28d25af2
yann <yann@10.3.62.35>
2024-12-18 10:01:40 +0100

    Continuation de Partie 3

--- PremierprojetTest/src/premierprojettest/Partie.java
+++ PremierprojetTest/src/premierprojettest/Partie.java
@@ -1,14 +1,32 @@
-package premierprojettest;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package premierprojettest;
 
+import java.util.ArrayList;
+import premierprojettest.Combinaison;
+//import PlateauDeJeu;
 /**
  *
- * @author yann
+ * @author kango
  */
 public class Partie {
+    PlateauDeJeu plateau;
+    ArrayList <Character> couleursDisponibles; //liste de toutes les couleures dispo
+    //PlateauDeJeu plateau; //permet de gérer les tentatives de reponses
     
+    
+    public Partie(PlateauDeJeu plateau, ArrayList <Character> couleursDisponibles, int taille) {
+        for(int i=0; i<taille; i++){
+            System.out.println();
+        }
+    }
 }
+    
+    
+    
+    
+    
+    
+
